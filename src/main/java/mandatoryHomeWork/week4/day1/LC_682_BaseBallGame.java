e1f63c73b82d55fb825b557ed7b449187b2ad12b
krishnabharathi123 <60067388+krishnabharathi123@users.noreply.github.com>
2023-06-30 12:03:32 +0530

    Update LC_682_BaseBallGame.java

--- src/main/java/mandatoryHomeWork/week4/day1/LC_682_BaseBallGame.java
+++ src/main/java/mandatoryHomeWork/week4/day1/LC_682_BaseBallGame.java
@@ -71,4 +71,104 @@ public class LC_682_BaseBallGame {
 		}
 		System.out.println(sum);
 	}
+	//Using stack 
+	/*
+	 * If u find the numbers, push it in the stack
+	 * If u find C remove the pop element from stack
+	 * If u find D peek the last element and multiply with 2 and push it to the stack
+	 * If u find + peek last and last-1 element and add it together and push in to stack
+	 */
+	private void baseBallUsingStack(String[] ops) {
+		StackInt st=new StackInt();
+		int num;
+		int val;
+		int size=-1;
+		int sum=0;
+		for(int i=0;i<ops.length;i++) {
+			try {
+				num=Integer.parseInt(ops[i]);
+				//System.out.println(num);
+				st.push(num);
+			}
+			catch(Exception e){
+				char charAt = ops[i].charAt(0);
+				switch(charAt) {
+				//If you find the character 'C', pop from the stack
+				  case 'C':
+					  st.pop();
+						size--;
+				    break;
+				  //If you find the character 'D' peek the last value and multiply by 2 and push it into the stack
+				  case 'D':
+					  val=2*(st.peek(1));
+					  st.push(val);
+						size++;
+				    break;
+				  //If you find the character '+' peek the last and last-1 value and add it and push it into the stack
+				  case '+':
+						val= st.peek(1)+st.peek(2);
+						
+						 st.push(val);
+							
+							size++;
+					}
+			}
+		}
+		
+		//System.out.println(st.size());
+		for(int i=1;i<=st.size();i++) {
+			sum=sum+st.peek(i);
+		}
+		System.out.println(sum);
+		
+	}
+	private void baseBallUsingStack1(String[] ops) {
+		StackInt st=new StackInt();
+		int num;
+		int val;
+		int size=-1;
+		int sum=0;
+		for(int i=0;i<ops.length;i++) {
+			try {
+				num=Integer.parseInt(ops[i]);
+				//System.out.println(num);
+				st.push(num);
+			}
+			catch(Exception e){
+				char charAt = ops[i].charAt(0);
+				switch(charAt) {
+				//If you find the character 'C', pop from the stack
+				  case 'C':
+					  st.pop();
+						size--;
+				    break;
+				  //If you find the character 'D' peek the last value and multiply by 2 and push it into the stack
+				  case 'D':
+					  val=2*(st.peek());
+					  st.push(val);
+						size++;
+				    break;
+				  //If you find the character '+' pop the last and last-1 value,store in a variables and push it back to the stack
+				    //add both the values and push it into the stack
+				  case '+':
+					  int last=st.pop();
+					  int prev=st.pop();
+					  st.push(prev);
+					  st.push(last);
+						val= last+prev;
+						
+						 st.push(val);
+							
+							size++;
+					}
+			}
+		}
+		
+		//System.out.println(st.size());
+		for(int i=1;i<=st.size();i++) {
+			sum=sum+st.peek(i);
+		}
+		System.out.println(sum);
+		
+	}
 }
