cc9c68681949ce4d758803b421bd6149071c5d80
krishnabharathi123 <60067388+krishnabharathi123@users.noreply.github.com>
2023-07-02 07:14:00 +0530

    Update HC_CaesarCipher1.java

--- src/main/java/mandatoryHomeWork/week8/day5/HC_CaesarCipher1.java
+++ src/main/java/mandatoryHomeWork/week8/day5/HC_CaesarCipher1.java
@@ -10,9 +10,34 @@ public class HC_CaesarCipher1 {
 	public void example1() {
 		String s="middle-Outz";
 		int k=2;
-		caesarCipher(s,k);
+		caesarCipher1(s,k);
 	}
-	
+	public static String caesarCipher1(String s, int k) {
+    String output="";
+    char ch1;
+    int n=k;
+    for(int i=0;i<s.length();i++){
+        if(Character.isLetter(s.charAt(i))){
+        int ch=s.charAt(i);//120
+        while(n>0){
+                 ch = ch+1;//122
+        
+        if(ch==123 || ch==91) {
+            ch=ch-26;
+        }
+        n--;
+        }
+            n=k; 
+           ch1=(char)ch;
+            output=output+ch1;   
+        }
+        else{
+            output=output+s.charAt(i);
+        }
+        
+    }
+        return output;
+    }
 	
 	public static String caesarCipher(String s, int k) {
    
